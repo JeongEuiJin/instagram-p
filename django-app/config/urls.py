2ddda7fb4c0863e944797d872cc353d674824761
JeongEuiJin <hehar1020@gmail.com>
2017-06-19 01:08:42 +0900

    member login create

--- django-app/config/urls.py
+++ django-app/config/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^post/', include('post.urls')),
+                  url(r'^member/', include('member.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django-app/post/views.py
+++ django-app/post/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 
 from .models import Post
+
+User = get_user_model()
 
 
 def post_list(request):
@@ -24,3 +28,27 @@ def post_detail(request, post_pk):
         return redirect('post:post_list')
 
     return render(request, 'post/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        user = User.objects.first()
+        post = Post.objects.create(
+            author=user,
+            photo=request.FILES['file'],
+        )
+        comment_string = request.Post.get('comment', '')
+
+        if comment_string:
+            post.comment_set.create(
+                author=user,
+                content=comment_string,
+            )
+
+        return redirect('post:post_detail', post_pk=post.pk)
+    else:
+        return render(request, 'post/post_create.html')
+
+
+def post_anyway(request):
+    return redirect('post:post_list')
